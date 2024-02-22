8740f5fcc96bbdcb64e21a7649e1a212fe48d216
Soheli <farhana.engineer@gmail.com>
2024-02-22 14:18:39 -0500

    add coding to product class

---
+++ product.js
@@ -0,0 +1,21 @@
+class product{
+    //create constructor method
+    constructor(name, price, currency){
+        this.name = name;
+        this.price = price;
+        this.currency = currency;
+    }
+
+    //create get method
+    getProduct(){
+        return `Product Name: ${this.name} and price: ${this.currency}${this.price}`;
+    }
+}
+
+// object declation
+const product1 = new product("McBook", 2000, "$");
+console.log(product1.getProduct());
+
+const product2 = new product("iPad", 800, "£");
+console.log(product2.getProduct());
+
